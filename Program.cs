9ea71e2a49d4fb73627410272afdc2ce82581a18
shobhitsharma123 <63493884+shobhitsharma123@users.noreply.github.com>
2020-04-22 17:42:39 +0530

    10th commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exploring_Visual_Studio_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
